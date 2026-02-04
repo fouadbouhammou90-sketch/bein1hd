c4a88ad02f3ea97b4a5b9f57070b7f50143f0c3f
Rayantvapps <fouadbouhammou90@gmail.com>
2026-02-04 21:33:48 +0100

    Update bein1hd.js

--- bein1hd.js
+++ bein1hd.js
@@ -1,10 +1,17 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
+    const url = new URL(req.url, `http://${req.headers.host}`);
+    
+    // تأكد أن الرابط يحتوي على كلمة السر 2026
+    if (url.searchParams.get("key") !== "2026") {
+        res.writeHead(403);
+        res.end("Access Denied: Wrong Key");
+        return;
+    }
+
     const targetUrl = "http://app.upsdo.me:8080/live/PCCQTZPXVCEG/041212071179/93914.ts";
 
-    // إرسال أمر "تحويل" (302 Redirect)
-    // هذا يجعل التطبيق يفتح الرابط الأصلي مباشرة ولكن خلف اسم نطاقك
     res.writeHead(302, {
         'Location': targetUrl,
         'Access-Control-Allow-Origin': '*'
